117a44db6804fa4f156aa7dd134aa6e052319205
Alex Russell <ajex94@gmail.com>
2019-09-08 16:13:30 -0600

    Update Project.jsx File
    * update npm versions
    * make pages uses same h1
    * update title
    * update project dates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h2>Hello World!</h2>
+        <h1>Hello World!</h1>
         <div className="clearfix">
 
           <img className="profile" src={profile} alt="Yours truly" title="Hello there!"/>
           <p className="about">
           My name is Alexander Russell.
           I am a graduate of the University of Saskatchewan with a degree in Computer Science and a Minor in Mathematics. 
-          I am looking for a career that would facilitate my passion for learning and help me to grow as a developer.
+          I am a currently a developer at <a href="http://territorial.ca">Territorial</a> working on <a href="https://territorial.ca/case-studies/hugr">Hugr</a>.
           </p>
           I am a driven, easygoing individual who enjoys programming, whether it be with others or on my own.
           When I'm not working I spend my spare time, hiking, swimming, playing tennis, and video games.
