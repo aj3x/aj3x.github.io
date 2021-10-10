7755f9a6b61e4f7c56d6197e2f5e9eaa5b442e0d
Alex Russell <ajex94@gmail.com>
2021-10-10 12:43:42 -0600

    Update to Typescript (1/2)

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,13 @@ import './App.scss';
 import Foot from './components/Foot';
 import Nav from './components/Nav';
 
-class App extends PureComponent {
-  render() {
-    return (
-      <div className="container">
-        <Nav />
-        <Foot />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container">
+      <Nav />
+      <Foot />
+    </div>
+  );
 }
 
 export default App;
